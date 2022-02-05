3b2231c160d3d413e676cdba4459ba0e18cecdc5
wagner <wagner.talkback@gmail.com>
2022-02-04 23:28:13 -0300

    arquivos para iniciar o repositório

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("programa Tabuada 2");
+            for (int multiplicar = 1; multiplicar <= 10; multiplicar++)
+            {
+                for (int contar = 0; contar <= 10; contar++)
+                {
+                    Console.Write(multiplicar + "*" + contar + "=" + multiplicar * contar);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
